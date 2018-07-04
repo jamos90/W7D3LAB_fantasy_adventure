f120a0f64c36a5ec6bb693fa6e969a08bdfbad5e
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-04 12:04:13 +0100

    lunchtime commit

---
+++ src/test/java/SpellTest.java
@@ -0,0 +1,15 @@
+import Skills.Spell;
+import org.junit.Before;
+
+public class SpellTest {
+
+    Spell spell1;
+    Spell spell2;
+    Spell spell3;
+
+    @Before
+    public void setUp(){
+        spell =
+
+    }
+}
