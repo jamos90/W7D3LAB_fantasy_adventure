f294b353fbf61c4df5acb66d0244e8eeeac35c33
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-04 12:02:38 +0100

    all skills set up

--- src/main/java/Skills/Restoration.java
+++ src/main/java/Skills/Restoration.java
@@ -1,4 +1,8 @@
 package Skills;
 
-public class Restoration {
+public class Restoration extends Skill {
+
+    public Restoration(String name, String type, int damage){
+        super(name,type,damage);
+    }
 }
