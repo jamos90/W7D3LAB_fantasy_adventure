1dd226b0514c0a5680701e03ce73865e0fd71411
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-04 11:31:24 +0100

    knight created and tests passed

---
+++ src/main/java/Players/Fighters/Knight.java
@@ -0,0 +1,8 @@
+package Players.Fighters;
+
+public class Knight extends Players.Player {
+
+    public Knight(String name, int hp){
+        super(name, hp);
+    }
+}
---
+++ src/test/java/KnightTest.java
@@ -0,0 +1,28 @@
+import Players.Fighters.Knight;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+    public class KnightTest {
+        Knight knight;
+
+        @Before
+        public void setUp(){
+            knight = new Knight("Sir Galahad", 70);
+        }
+
+        @Test
+        public void hasName(){
+            assertEquals("Sir Galahad", knight.getName());
+        }
+
+        @Test
+        public void hasHP(){
+            assertEquals(70, knight.getHP());
+        }
+
+    }
+
+
