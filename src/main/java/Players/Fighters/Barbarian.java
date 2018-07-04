ef8ca6e60ec99d9ba281ffa77b897db0c1227764
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-04 11:28:42 +0100

    barbarian created and tests passing

---
+++ src/main/java/Players/Fighters/Barbarian.java
@@ -0,0 +1,10 @@
+package Players.Fighters;
+
+public class Barbarian extends Players.Player {
+
+    public Barbarian(String name, int hp){
+        super(name,hp);
+    }
+
+
+}
---
+++ src/test/java/BarbarianTest.java
@@ -0,0 +1,29 @@
+import Players.Fighters.Barbarian;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BarbarianTest {
+
+        Barbarian barbarian;
+
+        @Before
+        public void setUp(){
+            barbarian = new Barbarian("Conan", 90);
+        }
+
+        @Test
+        public void hasName(){
+            assertEquals("Conan", barbarian.getName());
+        }
+
+        @Test
+        public void hasHP(){
+            assertEquals(90, barbarian.getHP());
+        }
+
+    }
+
+
+
